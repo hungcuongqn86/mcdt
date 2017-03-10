c7eb28e41fdded6de8f8a65a7a4dc873d1371a58
Nguyen Hung Cuong <hungcuongqn86@gmail.com>
2017-03-11 05:37:15 +0700

    lib

--- application/logout/controllers/IndexController.php
+++ application/logout/controllers/IndexController.php
@@ -1,23 +1,21 @@
 <?php
-/**
- * Class Xu ly thong thong tin loai danh muc
- */
-class Logout_IndexController extends  Zend_Controller_Action {
-	public function init(){
 
-	}
-	public function indexAction(){
-		Zend_Loader::loadClass('Zend_Session');
-		//Load class Extra_Init
-		Zend_Loader::loadClass('Extra_Init');
-		Zend_Session::start();
-		Zend_Session::destroy();	
-		$sReURL = Extra_Init::_setUserLoginUrl();?>
-		<script>
-			window.location.href = '<?=$sReURL;?>';
-		</script>
-		<?php
+/**
+ * Class Logout_IndexController
+ */
+class Logout_IndexController extends Zend_Controller_Action
+{
+    public function init()
+    {
+        Zend_Session::start();
+        Zend_Session::destroy();
+        $sReURL = Extra_Init::_setUserLoginUrl(); ?>
+        <script>
+            window.location.href = '<?=$sReURL;?>';
+        </script>
+        <?php
         exit;
-	}
+    }
 }
+
 ?>
