76da1e2470f203241eda4769fbb9137e3ad6db7e
Nguyen Hung Cuong <hungcuongqn86@gmail.com>
2017-03-11 19:14:44 +0700

    print

--- application/record/controllers/receiveController.php
+++ application/record/controllers/receiveController.php
@@ -1815,6 +1815,31 @@ class record_receiveController extends  Zend_Controller_Action {
                             $sValue = "ngày ".date("d", strtotime($sValue))." tháng ".date("m", strtotime($sValue))." năm ".date("Y", strtotime($sValue));
                         }
                     }*/
+                    if ($sDataFormat == "formfieldata"){
+                        $sBlockName = $arrElement["sblockname"];
+                        $sfind_string_list = $arrElement["find_string_list"];
+                        $arrfind_string = explode(',',$sfind_string_list);
+                        $sfield_list = $arrElement["field_list"];
+                        $arrfield = explode(',',$sfield_list);
+                        $arrDocx = array();
+                        $sValue = htmlspecialchars_decode($sValue);
+                        $arrValue = json_decode($sValue);
+                        if($arrValue){
+                            foreach ($arrValue as $index => $value) {
+                                $arrTemp = array();
+                                for($i=0;$i<sizeof($arrfind_string);$i++){
+                                    $key = '#'.$arrfind_string[$i].'#';
+                                    $objkey = $sXmlTagInDb.'_'.$arrfield[$i].'_'.$index;
+                                    //echo $objkey;
+                                    $arrTemp[$key] = $value->$objkey;
+                                }
+                                array_push($arrDocx, $arrTemp);
+                            }
+                        }
+                        //var_dump($arrDocx);exit;
+                        $phpdocx->assignBlock($sBlockName, $arrDocx);
+                    }
+
                     //Kieu du lieu muc dich su dung dat
                     if ($sDataFormat == "datatable"){
                         $sBlockName = $arrElement["sblockname"];
