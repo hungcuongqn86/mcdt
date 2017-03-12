76cc0105fadaaf35c780d5c4a88e6fabb44cfb9b
Nguyen Hung Cuong <hungcuongqn86@gmail.com>
2017-03-13 00:16:40 +0700

    xml

--- library/Extra/Xml.php
+++ library/Extra/Xml.php
@@ -867,8 +867,15 @@ class Extra_Xml extends RAX
                 $spRetHtml = $spRetHtml . $this->note;
                 $spRetHtml .= $note;
                 break;
+            case "selectbox_sex";
+                $spRetHtml .= $v_str_label;
+                $spRetHtml .= '<select id="'.$inputid.'" name="'.$inputid.'" class="normal_textbox formfielgen ' . $sformFielName . '_formfielgen_#row" style="width:' . $width . '" ' . self::_generateEventAndFunction($js_function_list, $js_action_list) . '" >';
+                $spRetHtml .= '<option id="Ông" name="Ông" value="Ông">Ông</option>';
+                $spRetHtml .= '<option id="Bà" name="Bà" value="Ông">Bà</option>';
+                $spRetHtml .= "</select>";
+                break;
             default:
-                $spRetHtml = $styleLabel;
+                $spRetHtml .= $styleLabel;
         }
         return $spRetHtml;
     }
